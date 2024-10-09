0e6f9ee225398fc4a6b6c5270a77e9961ad2e286
Panda248 <p4nda248@gmail.com>
2024-10-09 10:31:56 -0400

    implement browse for directory

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
-import shutil
+import shutil, os
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
 from shutil import *
 
 
@@ -8,22 +9,44 @@ imgname = r"4weirdegg.png"
 srcpath = r"from/"
 directory1 = r"dir1/"
 directory2 = r"dir2/"
+files = list()
 
 
 def moveFile(directory) :
     shutil.move(srcpath + imgname, directory + imgname)
 
+def defineDirectories() :
+    directory1 = filedialog.askdirectory(parent = root, initialdir="/", title = "Directory 1")
+    directory2 = filedialog.askdirectory(parent = root, initialdir="/", title = "Directory 2")
 
+def defineSource() :
+    srcpath =filedialog.askdirectorya(parent = root, initialdir = "/", title= "Source")
+
+def changeImage() -> bool:
+    if(files.len() > 0) :
+        imgname = os.listdir(srcpath)[0]
+        return True
+    return False
+        
+def init() :
+    files = os.listdir(srcpath)
+    
+    
 root = Tk()
 root.title("Image Sorter")
+
 frame = ttk.Frame(root, padding="1 1 1 1", width = 100, height = 100)
 frame.grid(column=0, row=0, sticky=(N,W,E,S))
+
 root.columnconfigure(0, weight = 1)
 root.rowconfigure(0, weight=1)
 
 title = ttk.Label(frame, text="Image Sorter", )
+
 img = PhotoImage(file=srcpath + imgname)
+
 image = ttk.Label(frame, image = img).grid(column = 1, row=1, columnspan = 2)
+
 button = ttk.Button(frame, text = directory1, command= lambda : moveFile(directory1)).grid(column = 1, row = 2)
 button2 = ttk.Button(frame, text = directory2, command = lambda : moveFile(directory2)).grid(column = 2, row = 2)
 
