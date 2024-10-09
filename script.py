1349329f939a59620b8841c543bdd1ad7dcf480e
Panda248 <p4nda248@gmail.com>
2024-10-09 18:01:52 -0400

    functional

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 from shutil import *
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image, ImageFile
 
 #declare vars
 imgname = r"4weirdegg.png"
@@ -21,45 +21,62 @@ title : Label
 frame : Frame
 dir1 : Button
 dir2 : Button
-
+dirBrowse : Button
+srcBrowse : Button
 
 
 def moveFile(directory) :
     shutil.move(srcpath + imgname, directory + imgname)
+    fileList.remove(fileList[0])
     if(changeImageFile()) :
         changeImage()
 
 def defineDirectories() :
-    global directory1, directory2 
+    global directory1, directory2, dir1, dir2
 
-    directory1 = filedialog.askdirectory(parent = root, initialdir="/", title = "Directory 1")
-    directory2 = filedialog.askdirectory(parent = root, initialdir="/", title = "Directory 2")
+    directory1 = filedialog.askdirectory(parent = root, initialdir="/", title = "Directory 1") + "/"
+    directory2 = filedialog.askdirectory(parent = root, initialdir="/", title = "Directory 2") + "/"
+    
+    dir1.configure(text = directory1)
+    dir1.text = directory1
+    dir2.configure(text= directory2)
+    dir2.text = directory2
+    
 
 def defineSource() :
-    global srcpath
+    global srcpath, fileList, srcBrowse
 
-    srcpath = filedialog.askdirectorya(parent = root, initialdir = "/", title= "Source")
+    srcpath = filedialog.askdirectory(parent = root, initialdir = "/", title= "Source") + "/"
+    fileList = os.listdir(srcpath)
+    
+    srcBrowse.configure(text = srcpath)
+    srcBrowse.text = srcpath
+    
+    changeImageFile()
+    changeImage()
 
 def changeImageFile() -> bool:
     global imgname
-    fileList.remove(fileList[0])
+    while(not (fileList[0].endswith(".png") or fileList[0].endswith(".PNG") or fileList[0].endswith(".jpg") or fileList[0].endswith(".JPG"))) :
+        fileList.remove(fileList[0])
+    
     if(len(fileList) > 0) :
-        imgname = os.listdir(srcpath)[0]
-        print(imgname)
+        imgname = fileList[0]
+        print(srcpath + imgname)
         return True
     return False
         
 def changeImage() :
     global img, image
-    img = ImageTk.PhotoImage(Image.open(srcpath + imgname))
-    image = ttk.Label(frame, image = img)
-    image.pack()
-    
+    img = ImageTk.PhotoImage(Image.open(srcpath + imgname).resize((500,500)))
+    image.configure(image=img)
+    image.image = img
 
 def init() :
     #init vars
-    global fileList, root, img, image, title, frame, dir1, dir2
+    global fileList, root, img, image, title, frame, dir1, dir2, dirBrowse, srcBrowse
 
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
     fileList = os.listdir(srcpath)
     changeImageFile()
 
@@ -76,15 +93,21 @@ def init() :
     title.grid(column = 1, row = 0, columnspan = 2)
 
     #image
-    img = ImageTk.PhotoImage(Image.open(srcpath+imgname))
+    img = ImageTk.PhotoImage(Image.open(srcpath+imgname).resize((500,500)))
     image = ttk.Label(frame, image = img)
     image.grid(column = 1, row=1, columnspan = 2)
 
-    #directory buttons
+    #sort buttons
     dir1 = ttk.Button(frame, text = directory1, command= lambda : moveFile(directory1))
     dir1.grid(column = 1, row = 2)
     dir2 = ttk.Button(frame, text = directory2, command = lambda : moveFile(directory2))
     dir2.grid(column = 2, row = 2)
+    
+    #source and directory browser buttons
+    dirBrowse = ttk.Button(frame, text = "Select Directories", command = defineDirectories)
+    dirBrowse.grid(row = 0, column = 0)
+    srcBrowse = ttk.Button(frame, text = srcpath, command = defineSource)
+    srcBrowse.grid(row = 0, column = 3)
 
 def main() :
     init()
