95020619b66569d1ea3c0e92e08f94f8b951d615
Panda248 <p4nda248@gmail.com>
2024-10-09 14:58:43 -0400

    testing functionality and image updating

--- script.py
+++ script.py
@@ -4,51 +4,90 @@ from tkinter import ttk
 from tkinter import filedialog
 from shutil import *
 
-
+#declare vars
 imgname = r"4weirdegg.png"
 srcpath = r"from/"
 directory1 = r"dir1/"
 directory2 = r"dir2/"
-files = list()
+fileList : list[str]
+
+#declare gui vars
+root : Tk
+img : PhotoImage
+image : Label
+title : Label
+frame : Frame
+dir1 : Button
+dir2 : Button
+
 
 
 def moveFile(directory) :
     shutil.move(srcpath + imgname, directory + imgname)
+    if(changeImageFile()) :
+        changeImage()
 
 def defineDirectories() :
+    global directory1, directory2 
+
     directory1 = filedialog.askdirectory(parent = root, initialdir="/", title = "Directory 1")
     directory2 = filedialog.askdirectory(parent = root, initialdir="/", title = "Directory 2")
 
 def defineSource() :
-    srcpath =filedialog.askdirectorya(parent = root, initialdir = "/", title= "Source")
+    global srcpath
 
-def changeImage() -> bool:
-    if(files.len() > 0) :
+    srcpath = filedialog.askdirectorya(parent = root, initialdir = "/", title= "Source")
+
+def changeImageFile() -> bool:
+    global imgname
+    fileList.remove(fileList[0])
+    if(len(fileList) > 0) :
         imgname = os.listdir(srcpath)[0]
+        print(imgname)
         return True
     return False
         
+def changeImage() :
+    global img
+    newImg = PhotoImage(srcpath + imgname)
+    img = newImg
+    image.config(image = newImg)
+    
+
 def init() :
-    files = os.listdir(srcpath)
-    
-    
-root = Tk()
-root.title("Image Sorter")
+    #init vars
+    global fileList, root, img, image, title, frame, dir1, dir2
 
-frame = ttk.Frame(root, padding="1 1 1 1", width = 100, height = 100)
-frame.grid(column=0, row=0, sticky=(N,W,E,S))
+    fileList = os.listdir(srcpath)
+    changeImageFile()
 
-root.columnconfigure(0, weight = 1)
-root.rowconfigure(0, weight=1)
+    #root and main frame
+    root = Tk()
+    root.title("Image Sorter")
+    frame = ttk.Frame(root, padding="1 1 1 1", width = 100, height = 100)
+    frame.grid(column=0, row=0, sticky=(N,W,E,S))
+    root.columnconfigure(0, weight = 1)
+    root.rowconfigure(0, weight=1)
 
-title = ttk.Label(frame, text="Image Sorter", )
+    #title
+    title = ttk.Label(frame, text="Image Sorter")
+    title.grid(column = 1, row = 0, columnspan = 2)
 
-img = PhotoImage(file=srcpath + imgname)
+    #image
+    img = PhotoImage(file=srcpath+imgname)
+    image = ttk.Label(frame, image = img)
+    image.grid(column = 1, row=1, columnspan = 2)
 
-image = ttk.Label(frame, image = img).grid(column = 1, row=1, columnspan = 2)
+    #directory buttons
+    dir1 = ttk.Button(frame, text = directory1, command= lambda : moveFile(directory1))
+    dir1.grid(column = 1, row = 2)
+    dir2 = ttk.Button(frame, text = directory2, command = lambda : moveFile(directory2))
+    dir2.grid(column = 2, row = 2)
 
-button = ttk.Button(frame, text = directory1, command= lambda : moveFile(directory1)).grid(column = 1, row = 2)
-button2 = ttk.Button(frame, text = directory2, command = lambda : moveFile(directory2)).grid(column = 2, row = 2)
+def main() :
+    init()
+    root.mainloop()
 
 
-root.mainloop()
+if __name__ == "__main__" : 
+    main()
